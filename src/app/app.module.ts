be58802e423dda93ac23ddae16327fa275f7e1f5
Daniel Brito <daniellimabrito@gmail.com>
2020-11-08 02:47:46 -0800

    Fixed DatePicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { QuotesComponent } from './quotes/quotes.component';
 import { AgendaFormComponent } from './agenda-form/agenda-form.component';
-// import { DatepickermonthComponent } from './datepickermonth/datepickermonth.component';
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { DatepickermonthComponent } from './datepickermonth/datepickermonth.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { DatePipe } from '@angular/common';
+import { AgendaService } from './_services/agenda.service';
 
 
 @NgModule({
@@ -33,7 +34,7 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     BrowserAnimationsModule,
     BsDatepickerModule.forRoot()
   ],
-  providers: [],
+  providers: [AgendaService, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
